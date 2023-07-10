9188d168988eeda8a5d7877bed0361b342cf1d85
Ar.kuzmin <ar.kuzmin@yandex.ru>
2023-07-10 20:35:21 +0700

    Initial commit

---
+++ src/main/java/com/example/RikMastersTest/RikMastersTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.RikMastersTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikMastersTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikMastersTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RikMastersTest/RikMastersTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RikMastersTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RikMastersTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
